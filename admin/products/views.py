79ca9f7a2885af466cd9ce630800948df5c63bc2
Saim Haider <saimhaider26@Gmail.com>
2026-02-04 22:31:17 +0500

    database setup

---
+++ admin/products/views.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+        pass
+      
+
+    def create(self, request):
+        pass
+    
+
+
+
